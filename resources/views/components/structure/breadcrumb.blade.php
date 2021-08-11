e95c81aec23150774b7a16481947f0c221c66457
Darren Justin Wu <11906239@benilde.edu.ph>
2021-08-11 20:00:28 +0800

    added blade components for breadcumbs links

---
+++ resources/views/components/structure/breadcrumb.blade.php
@@ -0,0 +1,8 @@
+@php
+
+@endphp
+
+<ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+    <li class="breadcrumb-item"><a href="#"><i {{ $attributes(['class' => $classes ?? $icon  ])}}></i></a></li>
+    <li class="breadcrumb-item"><a href="#">{{$text}}</a></li>
+</ol>
